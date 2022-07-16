18db01141e22f764691d59f7845dff99b0b8d615
jameelappsintegra <mail4jameelmca@gmail.com>
2022-07-16 20:26:10 +0530

    changes

---
+++ server/server.js
@@ -0,0 +1,99 @@
+const { MongoClient } = require("mongodb");
+
+async function main() {
+  const uri =
+    "mongodb+srv://jameel1234:jameel1234@cluster0.yvees.mongodb.net/?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+
+    await createListing(client, {
+      name: "Lovely Loft",
+      summary: "A charming loft in Paris",
+      bedrooms: 1,
+      bathrooms: 1,
+    });
+
+    await createMultipleListings(client, [
+      {
+        name: "Infinite Views",
+        summary: "Modern home with infinite views from the infinity pool",
+        property_type: "House",
+        bedrooms: 5,
+        bathrooms: 4.5,
+        beds: 5,
+      },
+      {
+        name: "Private room in London",
+        property_type: "Apartment",
+        bedrooms: 1,
+        bathroom: 1,
+      },
+      {
+        name: "Beautiful Beach House",
+        summary:
+          "Enjoy relaxed beach living in this house with a private beach",
+        bedrooms: 4,
+        bathrooms: 2.5,
+        beds: 7,
+        last_review: new Date(),
+      },
+    ]);
+    await findOneListingByName(client, "Infinite Views");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await client.close();
+  }
+}
+main().catch(console.error);
+
+async function createListing(client, newListing) {
+  const result = await client
+    .db("sample_weatherdata")
+    .collection("listingsAndReviews")
+    .insertOne(newListing);
+  console.log(
+    `New listing created with the following id: ${result.insertedId}`
+  );
+}
+
+async function createMultipleListings(client, newListings) {
+  const result = await client
+    .db("sample_weatherdata")
+    .collection("listingsAndReviews")
+    .insertMany(newListings);
+
+  console.log(
+    `${result.insertedCount} new listing(s) created with the following id(s):`
+  );
+  console.log(result.insertedIds);
+}
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db("sample_weatherdata")
+    .collection("listingsAndReviews")
+    .findOne({ name: nameOfListing });
+
+  if (result) {
+    console.log(
+      `Found a listing in the collection with the name '${nameOfListing}':`
+    );
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
